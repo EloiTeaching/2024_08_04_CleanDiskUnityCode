2d346767de7fa8c7595036b2c20e0070422a4e22
Eloi Strée <eloistree@gmail.com>
2024-10-12 20:43:52 +0200

    Save: 10/12/2024 8:43 PM

---
+++ 2024_10_08_SharpStreetAR/NewBehaviourScript2.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+}
